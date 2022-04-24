a71841190681446cd12a0e46ba70bb8c749f534b
sparib <sparibtheonly@gmail.com>
2022-04-24 00:52:24 -0400

    debug

--- python/main.py
+++ python/main.py
@@ -39,11 +39,11 @@ class BotClient(discord.Client):
         self.member: discord.Member = None
 
     async def on_ready(self):
-        logger.info(config)
-        logger.info(trello)
         await self.wait_until_ready()
         logger.info("Successfully logged in to " + self.user.name)
         self.member = self.get_guild(config["GUILD_ID"]).get_member(config["USER_ID"])
+        logger.info(config)
+        logger.info(trello)
         if self.member is None or self.member is None: raise Exception("ID for Sparib no worko")
         logger.info("Cached user 362355607367581716: " + self.member.name)
 
